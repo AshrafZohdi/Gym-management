ae7258d03754d10d21d7e7e98ed300dfa4d8af5a
Mohammad Ashraf <mohammadashraf2001@gmail.com>
2020-11-28 21:37:12 +0100

    Main testing

--- main.java
+++ main.java
@@ -1,9 +1,15 @@
 
-
 public class main
 {
     public static void main(String[] args)
     {
+    	Member M1 = new Member("John","Smith",01/08/1974,"johnSmith@email.com",12/03/2019,12/05/2019,0,2,7,3);
+        System.out.println(M1);
+    	
+        Member M2 = new Member("Mark","Cena",19/5/1999,"MarkCena@email.com",11/06/2018,1,1,3,3);
+        System.out.println(M2);
+
+        Member M3 = new Member("Louis","Collins",22/02/2001,"LouisCollins@email.com",1,5,3,1);
+        System.out.println(M3);
         
-    }
 }

f0ac60227852b0ebe53fd19a9d996d70aa94e3fe
mohachmadim <72310049+mohachmadim@users.noreply.github.com>
2020-11-28 22:20:49 +0100

    ...

--- WorkoutClass.java
+++ WorkoutClass.java
@@ -9,14 +9,12 @@ public class WorkoutClass{
     public LocalDate startDate;
     public LocalDate endDate;
     public Coach coach;
-    //public int[][] filledSlotsIndex;
-    // {[1,0], [1,1], ...}
-    List<int[]> filledSlotsIndex = new ArrayList<int[]>();
+    public List<int[]> filledSlotsIndex = new ArrayList<int[]>(); // {[1,0], [1,1], ...} has the addresses that contain true
 	
     public WorkoutClass(List<int[]> index, String className, String description, LocalDate startDate, LocalDate endDate,
 			Coach coach) {
-    	resetCalender();
-    	fillClassSchedule(index);
+    	updateClassSchedule(index);
+    	copyList(index, this.filledSlotsIndex);
 		this.className = className;
 		this.description = description;
 		this.startDate = startDate;
@@ -24,6 +22,16 @@ public class WorkoutClass{
 		this.coach = coach;
 	}
     
+    /*
+     * function to copy the index from the constructor to filled slot index
+     */
+    
+    public void copyList(List<int[]> copyThis, List<int[]> emptyList) {
+
+    	for(int[] element: copyThis) {
+    		emptyList.add(element);
+    	}
+    }
     
 
     public void resetCalender() {
@@ -34,7 +42,8 @@ public class WorkoutClass{
     	}
     }
     
-    public void fillClassSchedule(List<int[]> index){
+    public void updateClassSchedule(List<int[]> index){
+    	resetCalender();
     	for (int[] address : index) {
     		this.weeklySchedule[address[0]][address[1]] = true;
     	}
